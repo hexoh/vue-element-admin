683f0e7e5d9055c2cba6a0f3e026e3dfced56186
hxh <allenxiaohu@gmail.com>
2026-02-11 17:02:53 +0800

    调整项目结构

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,8 +2,10 @@ import { globalIgnores } from 'eslint/config'
 import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
 import pluginVue from 'eslint-plugin-vue'
 import pluginVitest from '@vitest/eslint-plugin'
-import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+// import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 import pluginOxlint from 'eslint-plugin-oxlint'
+import eslintConfigPrettier from 'eslint-config-prettier' // 禁用冲突规则
+import eslintPluginPrettier from 'eslint-plugin-prettier' // 集成 Prettier
 
 // To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
 // import { configureVueProject } from '@vue/eslint-config-typescript'
@@ -13,7 +15,7 @@ import pluginOxlint from 'eslint-plugin-oxlint'
 export default defineConfigWithVueTs(
   {
     name: 'app/files-to-lint',
-    files: ['**/*.{vue,ts,mts,tsx}'],
+    files: ['**/*.{vue,js,ts,mts,tsx}']
   },
 
   globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
@@ -23,10 +25,23 @@ export default defineConfigWithVueTs(
 
   {
     ...pluginVitest.configs.recommended,
-    files: ['src/**/__tests__/*'],
+    files: ['src/**/__tests__/*']
   },
 
-  skipFormatting,
+  // skipFormatting, // 与Prettier配合，避免格式规则冲突
+  // 1. 先禁用与 Prettier 冲突的 ESLint 规则
+  eslintConfigPrettier,
 
-  ...pluginOxlint.configs['flat/recommended'],
+  // 2. 添加 Prettier 插件
+  {
+    plugins: {
+      prettier: eslintPluginPrettier
+    },
+    rules: {
+      // 启用 Prettier 格式化规则
+      'prettier/prettier': 'error'
+    }
+  },
+
+  ...pluginOxlint.configs['flat/recommended']
 )
