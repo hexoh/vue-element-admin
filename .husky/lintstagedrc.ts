a10b98cfe637eda7567eaec90ed779119569f0e8
hxh <allenxiaohu@gmail.com>
2026-07-27 11:16:06 +0800

    fix: 更新lintstagedrc和package.json中的oxlint配置

--- .husky/lintstagedrc.ts
+++ .husky/lintstagedrc.ts
@@ -1,8 +1,8 @@
 export default {
-  '*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write'],
+  '*.{js,jsx,ts,tsx}': ['oxlint --fix', 'eslint --fix', 'prettier --write'],
   '{!(package)*.json,*.code-snippets,.!(browserslist)*rc}': ['prettier --parser json --write'],
   'package.json': ['prettier --write'],
-  '*.vue': ['prettier --write', 'stylelint --fix'],
+  '*.vue': ['oxlint --fix', 'prettier --write', 'stylelint --fix'],
   '*.{scss,less,styl,css,html}': ['stylelint --fix', 'prettier --write'],
   '*.md': ['prettier --write'],
   '*.hbs': ['prettier --write']
