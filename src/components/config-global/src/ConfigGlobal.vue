fe82273963c3ffde9ae5ac9c893281c826af659d
hxh <allenxiaohu@gmail.com>
2026-09-23 15:51:26 +0800

    feat: 添加 ConfigGlobal 组件及相关类型定义，支持主题色和响应式布局

---
+++ src/components/config-global/src/ConfigGlobal.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+import { provide, computed, watch, onMounted } from 'vue'
+import { propTypes } from '@/utils/propTypes'
+import { type ComponentSize, ElConfigProvider } from 'element-plus'
+import { useLocaleStore } from '@/stores/modules/locale'
+import { useWindowSize } from '@vueuse/core'
+import { useAppStore } from '@/stores/modules/app'
+import { setCssVar } from '@/utils'
+import { useDesign } from '@/hooks/web/useDesign'
+
+const { variables } = useDesign()
+
+const appStore = useAppStore()
+
+const props = defineProps({
+  size: propTypes.oneOf<ComponentSize>(['default', 'small', 'large']).def('default')
+})
+
+provide('configGlobal', props)
+
+// 初始化所有主题色
+onMounted(() => {
+  appStore.setCssVarTheme()
+})
+
+const { width } = useWindowSize()
+
+// 监听窗口变化
+watch(
+  () => width.value,
+  (width: number) => {
+    if (width < 768) {
+      if (!appStore.getMobile) {
+        appStore.setMobile(true)
+      }
+      setCssVar('--left-menu-min-width', '0')
+      appStore.setCollapse(true)
+      if (appStore.getLayout !== 'classic') {
+        appStore.setLayout('classic')
+      }
+    } else {
+      if (appStore.getMobile) {
+        appStore.setMobile(false)
+      }
+      setCssVar('--left-menu-min-width', '64px')
+    }
+  },
+  {
+    immediate: true
+  }
+)
+
+// 多语言相关
+const localeStore = useLocaleStore()
+
+const currentLocale = computed(() => localeStore.currentLocale)
+</script>
+
+<template>
+  <ElConfigProvider
+    :namespace="variables.elNamespace"
+    :locale="currentLocale.elLocale"
+    :message="{ max: 1 }"
+    :size="size"
+  >
+    <slot></slot>
+  </ElConfigProvider>
+</template>
