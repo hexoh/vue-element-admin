ab5a3f1a71aaa9a8d666d834680274fcb1fb3a46
hxh <allenxiaohu@gmail.com>
2026-09-24 16:24:43 +0800

    feat: 添加 Descriptions 组件及相关逻辑，支持描述信息展示

--- eslint.config.ts
+++ eslint.config.ts
@@ -11,8 +11,8 @@ import vueParser from 'vue-eslint-parser'
 import tseslint from 'typescript-eslint'
 
 // To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
-// import { configureVueProject } from '@vue/eslint-config-typescript'
-// configureVueProject({ scriptLangs: ['ts', 'tsx'] })
+import { configureVueProject } from '@vue/eslint-config-typescript'
+configureVueProject({ scriptLangs: ['ts', 'tsx'] })
 // More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
 
 export default defineConfigWithVueTs(
